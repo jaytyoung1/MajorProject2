b7fbfff19c4075592c28a5bd256ab84783fbeb4d
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-28 17:55:03 -0500

    Add thrust functionality
    up and down arrows move Ship forward and back

--- Major Project 2/Assets/_Scripts/PlayerInput.cs
+++ Major Project 2/Assets/_Scripts/PlayerInput.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerInput : MonoBehaviour {
+public class PlayerInput : MonoBehaviour
+{
+    ShipHandler sHandler;
+
+    Vector3 moveInput;
+    Vector3 rotInput;
+
+    //bool powered = true;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        sHandler = GetComponent<ShipHandler>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        //receive input
+        float thrust = Input.GetAxisRaw("Thrust");
+        //if (thrust > 0 || thrust < 0)
+        //{
+        //    powered = true;
+        //}
+
+        moveInput = new Vector3(0, 0, thrust);
+        rotInput = new Vector3(0, 0, 0);
 	}
+
+    void FixedUpdate()
+    {
+        //send input
+        sHandler.MoveInput(moveInput, rotInput);
+    }
 }
--- Major Project 2/Assets/_Scripts/ShipHandler.cs
+++ Major Project 2/Assets/_Scripts/ShipHandler.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShipHandler : MonoBehaviour {
+public class ShipHandler : MonoBehaviour
+{
+    Vector3 posInput;
+    Vector3 rotInput;
+    public Rigidbody rbody;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //bool powered = true;
+    float speed = 150.0f;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    public void MoveInput(Vector3 move, Vector3 rote)
+    {
+        posInput = move;
+        rotInput = rote;
+        //powered = power;
+
+        ActuallyMove();
+    }
+
+    void ActuallyMove()
+    {
+        if (posInput.z != 0)
+        {
+            speed = 150.0f;
+            rbody.drag = 10;
+        }
+        else
+        {
+            speed = 0;
+            rbody.drag = 0;
+        }
+
+        rbody.AddRelativeForce(posInput * speed);
+        rbody.AddRelativeTorque(rotInput);
+    }
 }
