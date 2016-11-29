3220c79d58766d324b7ca09640a66111bccd96fa
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-29 16:56:45 -0500

    Add explosion when bullet hits asteroid

---
+++ Major Project 2/Assets/_Scripts/Explosion.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    private Vector3 newPosition;
+
+    //public ParticleSystem explosion;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Asteroid"))
+        {
+            Destroy(coll.gameObject);
+            explode();
+        }
+    }
+
+    void explode()
+    {
+        Debug.Log("in explode function");
+        //ParticleSystem explo = GetComponent<ParticleSystem>();
+        // explo.Play();
+        //explosion.Play();
+        newPosition = gameObject.transform.position;
+        GameObject exp = (GameObject)Instantiate(explosionPrefab, newPosition, Quaternion.identity);
+        Destroy(this.gameObject);
+        //explosionPrefab.PlayAnimation();
+        Destroy(exp, 2.0f);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
