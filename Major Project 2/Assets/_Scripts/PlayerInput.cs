52444e7151a4ede6748f8c7ef6ab2cb16df17790
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-28 20:13:45 -0500

    Add pitch and yaw functionality

--- Major Project 2/Assets/_Scripts/PlayerInput.cs
+++ Major Project 2/Assets/_Scripts/PlayerInput.cs
@@ -21,14 +21,17 @@ public class PlayerInput : MonoBehaviour
     {
         //receive input
         float thrust = Input.GetAxisRaw("Thrust");
+
         float roll = Input.GetAxisRaw("Roll");
+        float yaw = Input.GetAxisRaw("Mouse X");
+        float pitch = Input.GetAxisRaw("Mouse Y");
         //if (thrust > 0 || thrust < 0)
         //{
         //    powered = true;
         //}
 
         moveInput = new Vector3(0, 0, thrust);
-        rotInput = new Vector3(0, 0, roll);
+        rotInput = new Vector3(pitch, yaw, roll);
 	}
 
     void FixedUpdate()
