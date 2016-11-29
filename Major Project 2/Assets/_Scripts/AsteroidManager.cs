e731a80a56a6cd88b48f7948997982443c2a1c3b
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-29 14:07:56 -0500

    Checkpoint

--- Major Project 2/Assets/_Scripts/AsteroidManager.cs
+++ Major Project 2/Assets/_Scripts/AsteroidManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class AsteroidManager : MonoBehaviour
 {
     public GameObject ship;
-    public GameObject asteroid;
+    public GameObject[] asteroids;
     public int totalAsteroids = 10;
     private Vector3 newPosition;
 
@@ -18,19 +18,20 @@ public class AsteroidManager : MonoBehaviour
     {
         for (int i = 0; i < totalAsteroids; i++)
         {
-            float newX = ship.gameObject.transform.position.x + Random.Range(-50, 50);
-            float newY = ship.gameObject.transform.position.y + Random.Range(-50, 50);
-            float newZ = ship.gameObject.transform.position.z + Random.Range(10, 50);
+            float newX = ship.gameObject.transform.position.x + Random.Range(-10.0f, 10.0f);
+            float newY = ship.gameObject.transform.position.y + Random.Range(-10.0f, 10.0f);
+            float newZ = ship.gameObject.transform.position.z + Random.Range(10.0f, 10.0f);
             newPosition = new Vector3(newX, newY, newZ);
             //Debug.Log(newPosition);
 
-            GameObject newAsteroid = (GameObject)Instantiate(asteroid, newPosition, Quaternion.identity);
-            newAsteroid.GetComponent<Rigidbody>().AddForce(new Vector3((Random.Range(-100, 100)), (Random.Range(-100, 100)), (Random.Range(-100, 100))));
+            GameObject newAsteroid = (GameObject)Instantiate(asteroids[Random.Range(0, 4)], newPosition, Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f)));
+            newAsteroid.GetComponent<Rigidbody>().AddForce(new Vector3((Random.Range(-200.0f, 200.0f)), (Random.Range(-200.0f, 200.0f)), (Random.Range(-200.0f, 200.0f))));
         }
     }
 
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    
 	}
 }
