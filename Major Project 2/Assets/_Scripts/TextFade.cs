d5feed487bb7437b92273eb8df5459605c2f072f
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-29 23:29:49 -0500

    Correct score text box
    wrong script attached

---
+++ Major Project 2/Assets/_Scripts/TextFade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+    public Text levelText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Fade");
+    }
+
+    IEnumerator Fade()
+    {
+        for (float f = 3f; f >= 0; f -= 0.02f)
+        {
+            Color c = levelText.color;
+            //Color c = renderer.material.color;
+            c.a = f;
+            levelText.color = c;
+            yield return null;
+        }
+    }
+}
