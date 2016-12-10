73b1a64417fc44b7326c2dd4a377b524fe389dd6
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-09 20:20:21 -0500

    Add function in MenuController to delete keys and restart scene

--- Major Project 2/Assets/_Scripts/MenuController.cs
+++ Major Project 2/Assets/_Scripts/MenuController.cs
@@ -21,4 +21,10 @@ public class MenuController : MonoBehaviour
     {
         SceneManager.LoadScene("HighScoresScene");
     }
+
+    public void resetHighScores()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("HighScoresScene");
+    }
 }
