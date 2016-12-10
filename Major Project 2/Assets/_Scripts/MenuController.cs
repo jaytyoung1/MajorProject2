18554713799d53c96060730bd33a842e268118bd
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-09 19:27:38 -0500

    Add WelcomeScene with Play button
    pressing the Play button loads the GameScene

---
+++ Major Project 2/Assets/_Scripts/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void loadGameScene()
+    {
+        PlayerPrefs.SetInt("Score", 0);
+        SceneManager.LoadScene("GameScene");
+    }	
+}
