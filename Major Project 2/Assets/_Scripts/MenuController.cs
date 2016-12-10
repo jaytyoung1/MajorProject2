e45b1dd7bd406802be25484e02705aea6c9b82cb
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-09 19:40:01 -0500

    Add HighScoreScene with button to go back to WelcomeScene

--- Major Project 2/Assets/_Scripts/MenuController.cs
+++ Major Project 2/Assets/_Scripts/MenuController.cs
@@ -4,9 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public void loadWelcomeScene()
+    {
+        PlayerPrefs.DeleteKey("Score");
+        PlayerPrefs.DeleteKey("Lives");
+        SceneManager.LoadScene("WelcomeScene");
+    }
+
     public void loadGameScene()
     {
         PlayerPrefs.SetInt("Score", 0);
         SceneManager.LoadScene("GameScene");
-    }	
+    }
+
+    public void loadHighScoresScene()
+    {
+        SceneManager.LoadScene("HighScoresScene");
+    }
 }
