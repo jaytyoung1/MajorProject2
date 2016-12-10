c6bccdde334cd127c40cece040c2dfe8f1c0a2b1
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-10 00:52:42 -0500

    Import planets/galaxies and add spawner to both levels

---
+++ Major Project 2/Assets/_Scripts/PlanetSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetSpawner : MonoBehaviour
+{
+    public GameObject ship;
+    public GameObject[] planets;
+    public int totalPlanets = 5;
+    private Vector3 newPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
+        spawnPlanets();
+	}
+	
+    void spawnPlanets()
+    {
+        for (int i = 0; i < totalPlanets; i++)
+        {
+            float newX = ship.gameObject.transform.position.x + Random.Range(-1000.0f, 1000.0f);
+            float newY = ship.gameObject.transform.position.y + Random.Range(-1000.0f, 1000.0f);
+            float newZ = ship.gameObject.transform.position.z + Random.Range(-1000.0f, 1000.0f);
+            newPosition = new Vector3(newX, newY, newZ);
+            GameObject spawnedPlanet = (GameObject)Instantiate(planets[Random.Range(0, 4)], newPosition, Quaternion.identity);
+        }
+    }
+}
